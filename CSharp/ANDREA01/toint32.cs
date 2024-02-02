6c2bb4d07101d14b8767894334468583909399fa
Natalia Keller <natalia.kellermacielfarah@stud.itsaltoadriatico.it>
2024-02-02 10:36:02 +0100

    added c sharp folder, style.css

---
+++ CSharp/ANDREA01/toint32.cs
@@ -0,0 +1,68 @@
+﻿namespace andreacorso;
+using System;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // string? n1;
+        // int i = 0;
+        
+        
+        // Console.WriteLine("Digita un numero \b");
+        // n1 = Console.ReadLine();
+        
+        
+        
+
+        // while(i<5){
+        //     if(i == 0 || i == 4){
+        //         Console.WriteLine("{0}{0}{0}", n1);
+                
+        //     } else{
+
+        //         Console.WriteLine("{0} {0}", n1);
+
+        //     }
+
+           
+        //     i++;
+        // }
+
+
+        // double tempC;
+        
+        // double tempF;
+        
+        // double tempK;
+        
+        // Console.WriteLine("Insert");
+        
+        // tempC = Convert.ToDouble(Console.ReadLine());
+        
+        // tempF = tempC*(1.8)+32;
+        // tempK = tempC+273;
+
+        // Console.WriteLine($"{tempC} c = {tempF}f = {tempK}");
+
+      
+       Console.WriteLine("Quanti interi vuoi inserire");
+       string? s = Console.ReadLine();
+       int qta = Convert.ToInt32(s);
+
+       int sum = 0;
+        for (int i = 0; i < qta; i++)
+        {
+            Console.Write("Numero: ");
+            string? stringa = Console.ReadLine();
+            sum += Convert.ToInt32(stringa);
+            
+        }
+
+       
+       double reslt = sum /(double)qta;
+    }
+    
+}
---
+++ CSharp/ES_01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+namespace ES_01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       /*  float test = 10.25f;
+        int a = 10, b = 20, somma;
+        somma = a+b;
+        Console.WriteLine("La somma di "+a+" e "+b+" è uguale a "+somma);
+        Console.WriteLine("la somma di {0} e {1} è uguale a {2}", a, b, somma); */
+        
+
+        // int number;
+        // Console.Write("Numero da convertire: ");
+        // number = Convert.ToInt32(Console.ReadLine()); // 
+        // Console.WriteLine("il numero {0} convertito in binario è {1} ", number, Convert.ToString(number, 2));
+        // Console.WriteLine("il numero {0} convertito in 8 è {1} ", number, Convert.ToString(number, 8));
+        // Console.WriteLine("il numero {0} convertito in 16 è {1} ", number, Convert.ToString(number, 16));
+
+        // decimal amount = 4546890m; // un letterale decimal - suffisso m
+        //Console.OutputEncoding = Encoding.UTF8;
+        // Console.WriteLine("Ammontare:{0:C}", amount);
+
+        int ch;
+        Console.Write("Inserisci il carattere minuscolo da convertire [ ]\b\b");
+        ch = Console.Read();
+        int ch_U = TransformaMaiusc(ch);
+        Console.WriteLine("Il carattere {0} viene trasformato in maiuscolo {1}", (char)ch, (char)ch_U);
+    }
+
+    
+    public static int TransformaMaiusc(int ch){
+        int charUP;
+        if(ch>= 'a'&& ch<='z'){
+            charUP = ch - 'a' + 'A';
+            return charUP;
+        }
+
+        return ch;
+    }
+
+}
