672e61e652b24c83d24986ac4d44f6534fab23be
Natália Keller <natalia.kellermacielfarah@stud.itsaltoadriatico.it>
2024-02-15 17:42:49 +0100

    Add files via upload

---
+++ C#/Exercise01/Exercise01/Program.cs
@@ -0,0 +1,21 @@
+﻿string path = @"c:\temp\Text.txt";
+
+string readText = File.ReadAllText(path);
+
+string[]? splitText = readText.Split(' ');
+int bigLength = splitText[0].Length;
+string bigWord = splitText[0];
+
+
+Console.WriteLine(splitText.Length + 1 + " Words");
+
+for (int i = 0; i < splitText.Length; i++)
+{
+    if (splitText[i].Length >= bigLength)
+    {
+        bigLength = splitText[i].Length;
+        bigWord = splitText[i];
+    }
+}
+
+Console.WriteLine($"\nThe biggest word has {bigLength} characters \nand the biggest word is: {bigWord}");
